3065bbc806f06ad43f4db87a23aaa642456b7a98
Phil <irons7654321@gmail.com>
2025-11-22 13:32:46 +0800

    Add catOptionals and mapOptinal

--- Sources/IRFunctionalPipeline/IRFunctionalPipeline.swift
+++ Sources/IRFunctionalPipeline/IRFunctionalPipeline.swift
@@ -3,13 +3,14 @@
 
 
 precedencegroup Semigroup {
-  associativity: right
-  higherThan: AdditionPrecedence
+    associativity: right
+    higherThan: AdditionPrecedence
+    lowerThan: MultiplicationPrecedence
 }
 
 precedencegroup HashRocket {
-  associativity: left
-  higherThan: Semigroup
+    associativity: left
+    higherThan: Semigroup
 }
 
 infix operator =>: HashRocket
@@ -17,17 +18,17 @@ infix operator =>: HashRocket
 infix operator |>: FunctionApplicationFlipped
 
 public func |> <A, B> (a: A, f: (A) -> B) -> B {
-  return f(a)
+    return f(a)
 }
 
 precedencegroup FunctionApplication {
-  associativity: left
-  higherThan: Semigroup
+    associativity: left
+    higherThan: Semigroup
 }
 
 precedencegroup FunctionApplicationFlipped {
-  associativity: left
-  higherThan: FunctionApplication
+    associativity: left
+    higherThan: FunctionApplication
 }
 
 infix operator <>: Semigroup
@@ -35,28 +36,44 @@ infix operator <>: Semigroup
 // MARK: Semigroup
 
 public protocol Semigroup {
-  static func <>(_: Self, _: Self) -> Self
+    static func <>(_: Self, _: Self) -> Self
 }
 
 // MARK: Monoid
 
 public protocol Monoid: Semigroup {
-  static var e: Self { get }
+    static var identity: Self { get }
 }
 
-public func joined<M: Monoid>(_ s: M) -> ([M]) -> M {
-  return { xs in
-    if let head = xs.first {
-      return xs.dropFirst().reduce(head) { accum, x in accum <> s <> x }
+public func joined<M: Monoid>(_ separator: M) -> ([M]) -> M {
+    return { ms in
+        guard let head = ms.first else {
+            return .identity
+        }
+        return ms.dropFirst().reduce(head) { accum, m in
+            accum <> separator <> m
+        }
     }
-    return .e
-  }
 }
 
 extension String: Monoid {
-  public static let e = ""
+    public static let identity = ""
 
-  public static func <>(lhs: String, rhs: String) -> String {
-    return lhs + rhs
-  }
+    public static func <>(lhs: String, rhs: String) -> String {
+        return lhs + rhs
+    }
+}
+
+public func id<A>(_ a: A) -> A {
+    return a
+}
+
+public func catOptionals<S: Sequence, A>(_ xs: S) -> [A] where S.Element == A? {
+    return xs |> mapOptional(id)
+}
+
+public func mapOptional<S: Sequence, A>(_ f: @escaping (S.Element) -> A?) -> (S) -> [A] {
+    return { xs in
+        xs.compactMap(f)
+    }
 }
--- Tests/IRFunctionalPipelineTests/IRFunctionalPipelineTests.swift
+++ Tests/IRFunctionalPipelineTests/IRFunctionalPipelineTests.swift
@@ -6,3 +6,14 @@ import Testing
     #expect("hello" == (["hello"] |> joined(" ")))
     #expect("hello world" == (["hello", "world"] |> joined(" ")))
 }
+
+struct SequenceTests {
+
+    @Test func testCatOptionals() {
+        #expect([1, 2, 3] == ([1, nil, 2, nil, 3] |> catOptionals))
+    }
+
+    @Test func testMapOptional() {
+        #expect([2] == ([1, 2, 3] |> mapOptional { $0 % 2 == 0 ? $0 : nil }))
+    }
+}
